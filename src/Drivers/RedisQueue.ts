1567733dd32cd9fc3a5bcc45054bd5f9a9acac31
Daria <daria.staferova@gmail.com>
2022-02-23 18:12:04 +0200

    chore(driver): remove code duplication

--- src/Drivers/RedisQueue.ts
+++ src/Drivers/RedisQueue.ts
@@ -1,6 +1,13 @@
 import BeeQueue from 'bee-queue'
 import { DriverContract, JobContract } from '@ioc:Cavai/Queue'
 
+const unwrap = (job) => ({
+  id: job.id,
+  payload: job.data,
+  progress: job.progress,
+  reportProgress: job.reportProgress,
+})
+
 export default class RedisQueue implements DriverContract {
   private queue = new BeeQueue(this.config.name)
 
@@ -14,12 +21,7 @@ export default class RedisQueue implements DriverContract {
    */
   public async add<T extends Record<string, any>>(payload: T): Promise<JobContract<T>> {
     const job = await this.queue.createJob<T>(payload).save()
-    return {
-      id: job.id,
-      payload: job.data,
-      progress: job.progress,
-      reportProgress: job.reportProgress,
-    }
+    return unwrap(job)
   }
 
   /**
@@ -29,15 +31,7 @@ export default class RedisQueue implements DriverContract {
    * which receives queued job
    */
   public process(cb) {
-    const remappedCallback = (data) => {
-      return cb({
-        id: data.id,
-        payload: data.data,
-        progress: data.progress,
-        reportProgress: data.reportProgress,
-      })
-    }
-
+    const remappedCallback = (job) => cb(unwrap(job))
     this.queue.process(remappedCallback)
   }
 
@@ -48,15 +42,6 @@ export default class RedisQueue implements DriverContract {
    */
   public async getJob(id: string | number): Promise<JobContract<any> | null> {
     const job = await this.queue.getJob(String(id))
-    if (!job) {
-      return null
-    }
-
-    return {
-      id: job.id,
-      payload: job.data,
-      progress: job.progress,
-      reportProgress: job.reportProgress,
-    }
+    return !job ? null : unwrap(job)
   }
 }
