9c357f5c521f85e3ebf0d1edc984a003326c57d6
Margus Kevin Sünter <kevin@synter.ee>
2023-03-28 12:44:19 +0300

    Update Dispatcher types

--- src/Dispatcher.ts
+++ src/Dispatcher.ts
@@ -1,55 +1,64 @@
-import { DateTime } from "luxon";
+import { DateTime } from 'luxon'
 
-export default class Dispatcher implements Promise<void> {
-  public then<TResult1 = T, TResult2 = never>(
+type DispatcherResult = void
+
+export default class Dispatcher implements Promise<DispatcherResult> {
+  public then<TResult1 = DispatcherResult, TResult2 = never>(
     onfulfilled?:
-      | ((value: T) => TResult1 | PromiseLike<TResult1>)
-      | undefined
-      | null,
+    | ((value: DispatcherResult) => TResult1 | PromiseLike<TResult1>)
+    | undefined
+    | null,
     onrejected?:
-      | ((reason: any) => TResult2 | PromiseLike<TResult2>)
-      | undefined
-      | null
+    | ((reason: any) => TResult2 | PromiseLike<TResult2>)
+    | undefined
+    | null
   ): Promise<TResult1 | TResult2> {
-    return this.exec().then(onfulfilled, onrejected);
+    return this.exec().then(onfulfilled, onrejected)
   }
 
   public catch<TResult = never>(
     onrejected?:
-      | ((reason: any) => TResult | PromiseLike<TResult>)
-      | undefined
-      | null
-  ): Promise<T | TResult> {
-    return this.exec().catch(onrejected);
+    | ((reason: any) => TResult | PromiseLike<TResult>)
+    | undefined
+    | null
+  ): Promise<DispatcherResult | TResult> {
+    return this.exec().catch(onrejected)
   }
 
-  public finally(onfinally?: (() => void) | undefined | null): Promise<T> {
-    return this.exec().finally(onfinally);
+  public finally (onfinally?: (() => void) | undefined | null): Promise<DispatcherResult> {
+    return this.exec().finally(onfinally)
   }
 
-  private availableAt: DateTime;
+  /**
+   * Required when Promises are extended
+   */
+  public get [Symbol.toStringTag] () {
+    return this.constructor.name
+  }
 
-  constructor(
+  private availableAt: DateTime
+
+  constructor (
     private classPath,
     private name,
     private queueManager,
     private data
   ) {}
 
-  public async exec() {
+  public async exec () {
     if (!this.classPath) {
-      throw new Error(`classPath param missing in ${this.name}`);
+      throw new Error(`classPath param missing in ${this.name}`)
     }
 
     let payload = {
       classPath: this.classPath,
       data: this.data,
-      version: "v1",
-    };
+      version: 'v1',
+    }
 
     await this.queueManager.store(this.classPath, payload, {
       availableAt: this.availableAt,
-    });
+    })
   }
 
   /**
@@ -57,8 +66,8 @@ export default class Dispatcher implements Promise<void> {
    *
    * @param time Time after what job will be available for execution
    */
-  public delay(time) {
-    this.availableAt = time;
-    return this;
+  public delay (time) {
+    this.availableAt = time
+    return this
   }
 }
