903024e0f4abcc923c047ac81fc54129861a0e8a
Margus Kevin Sünter <kevin@synter.ee>
2023-07-26 13:13:20 +0300

    Wrap job into transaction

--- src/Drivers/Database.ts
+++ src/Drivers/Database.ts
@@ -1,4 +1,4 @@
-import { DatabaseContract } from '@ioc:Adonis/Lucid/Database'
+import { DatabaseContract, TransactionClientContract } from '@ioc:Adonis/Lucid/Database'
 import { DateTime } from 'luxon'
 import SuperJSON from 'superjson'
 import { DatabaseDriverConfig, JobRecord, QueueDriver, StoreOptions } from '../types'
@@ -7,6 +7,8 @@ export default class DatabaseDriver implements QueueDriver {
   constructor(protected config: DatabaseDriverConfig, private database: DatabaseContract) {}
 
   public pollingDelay = this.config.pollingDelay || 2000
+
+  private trx: TransactionClientContract
 
   /**
    * Store job to database
@@ -22,14 +24,21 @@ export default class DatabaseDriver implements QueueDriver {
   /**
    * Get next job from database
    */
-  public getNext(): Promise<JobRecord | null> {
-    return this.database
+  public async getNext(): Promise<JobRecord | null> {
+    this.trx = await this.database.transaction()
+    const job = this.trx
       .from(this.config.tableName)
       .where('available_at', '<', DateTime.now().toSQL({ includeOffset: false }))
       .where({ failed: false })
       .forUpdate()
       .skipLocked()
       .first()
+
+    if (!job) {
+      await this.trx.commit()
+    }
+
+    return job
   }
 
   /**
@@ -47,29 +56,32 @@ export default class DatabaseDriver implements QueueDriver {
    * Re-schedule job (update attempts and available_at) in Database
    */
   public async reSchedule(job: JobRecord, retryAfter: number) {
-    await this.database
+    await this.trx
       .from(this.config.tableName)
       .where({ id: job.id })
       .update({
         attempts: job.attempts,
         available_at: DateTime.now().plus({ seconds: retryAfter }),
       })
+    await this.trx.commit()
   }
 
   /**
    * Mark job as failed in database
    */
   public async markFailed(job: JobRecord) {
-    await this.database.from(this.config.tableName).where({ id: job.id }).update({
+    await this.trx.from(this.config.tableName).where({ id: job.id }).update({
       failed: true,
       attempts: job.attempts,
     })
+    await this.trx.commit()
   }
 
   /**
    * Remove job from database
    */
   public async remove(id: number | string): Promise<void> {
-    await this.database.from(this.config.tableName).where({ id: id }).delete()
+    await this.trx.from(this.config.tableName).where({ id: id }).delete()
+    await this.trx.commit()
   }
 }
